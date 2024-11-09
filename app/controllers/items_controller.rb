1a6ba12c59a4bfbe244ee1a5ca28dedf26f64440
Max Temkin <Max.Temkin@Gmail.com>
2024-11-09 17:16:43 +0000

    first commit

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -4,4 +4,18 @@ class ItemsController < ApplicationController
 
     render({ :template => "item_templates/list" })
   end
+
+  def links
+    render({ :template => "item_templates/backdoor" })
+  end
+
+  def new_items
+    x = Item.new
+    x.link_description = params[:link_url]
+    x.link_url = params[:description]
+    x.thumbnail_url = [:image_url]
+    x.save
+    redirect_to("/")
+  end
+
 end
